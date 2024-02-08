26830c0cde639b0b75f7dcc3f4a78833e2ef8dad
B Sonali Priyadarsani <140884611+SonaliPriyadarsani@users.noreply.github.com>
2024-02-08 22:32:41 +0530

    Tunehub First Version

---
+++ com.tunehub/src/main/java/com/tunehub/repositories/PlaylistRepository.java
@@ -0,0 +1,11 @@
+package com.tunehub.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tunehub.entity.Playlist;
+
+public interface PlaylistRepository 
+		extends JpaRepository<Playlist, Integer>
+{
+
+}
